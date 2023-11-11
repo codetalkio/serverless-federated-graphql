2e640b2773787e07e656b032e8061af4d40ed59f
Christian Kjær <ckl@codetalk.io>
2023-11-12 00:38:57 +0100

    Use tokio's mutex

--- lambda-directly-optimized/src/main.rs
+++ lambda-directly-optimized/src/main.rs
@@ -4,7 +4,8 @@ use lambda_http::{run, service_fn, Error, IntoResponse, Request};
 use reqwest::header::CONTENT_TYPE;
 use std::env;
 use std::fs;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tower::Service;
 use tracing::info;
 
@@ -15,15 +16,13 @@ async fn handle_request(
     let body = event.body();
     let event_payload: apollo_router::graphql::Request =
         serde_json::from_slice(body).expect("invalid graphql request");
-    info!("👉 Proxying request to router: {:?}", event_payload);
-    println!("👉 Proxying request to router: {:?}", event_payload);
+    info!("Proxying request to router: {:?}", event_payload);
 
     let builder = supergraph::Request::fake_builder()
         .header(CONTENT_TYPE, "application/json")
         .query(event_payload.query.clone().unwrap_or("".to_string()))
         .variables(event_payload.clone().variables)
         .extensions(event_payload.clone().extensions);
-    // TODO: Avoid this weird builder hack.
     let request = if let Some(operation_name) = event_payload.clone().operation_name {
         builder.operation_name(operation_name).build().unwrap()
     } else {
@@ -33,7 +32,7 @@ async fn handle_request(
     // Lock the supergraph for the brief time we are calling it. This will have no real-world
     // impact on performance because the Lambda only ever serves one request at a time.
     let response = {
-        let mut s = supergraph.lock().unwrap();
+        let mut s = supergraph.lock().await;
         s.call(request.try_into().unwrap())
     };
     let mut r = response.await?;
@@ -42,8 +41,7 @@ async fn handle_request(
     // the rest of the work for us to shape it into a nice Lambda response.
     let resp: serde_json::Value =
         serde_json::from_slice(r.next_response().await.unwrap().unwrap().to_vec().as_slice())?;
-    info!("👉 Deserialized Response: {:?}", resp);
-    println!("👉 Deserialized Response: {:?}", resp);
+    info!("Deserialized Response: {:?}", resp);
 
     Ok(resp)
 }
@@ -89,10 +87,6 @@ async fn handler() -> Result<(), Error> {
         .with_subgraph_network_requests()
         .build_router()
         .await?;
-
-    // Wait until the service is ready via ServiceExt::ready. This keeps us in the initialization
-    // phase, which typically has more resources allocated to it.
-    // supergraph.ready().await?;
 
     // Set up the Lambda event handler, wrap our supergraph in Arc(Mutex(..)), and so
     // we can safely pass it across async boundaries.
